87f934e55074edbd3549bcb5dc9d6ae97fd04d86
Agustin Amenabar <agustin.amenabar@westwing.de>
2017-01-06 17:25:36 +0100

    Added the provisioning files and updated the description

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# digitalocean-provision
-Provisioning scripts for digitalocean
+# Provisioning scripts for Digitalocean
+
+This pair of scripts will help you setup a server on a Dropplet in 5 minutes.
+
+Paste the contents of the **[cloud-init-on-ubuntu.yml](https://raw.githubusercontent.com/Flyimg/DigitalOcean-provision/master/cloud-init-on-ubuntu.yml)** file on to the "User data" field when creating a new Ubuntu Dropplet.
+
+Ideally you should also add any publick key you want to the root user snd the provisioning script will add it also to the first user so you can SSH in to the server once the dropplet is created.
---
+++ provision-flyimg-on-ubuntu.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Commented provisioning script for a flyimg server
+# Created for Ubuntu 16 but works with 14 and possibly with other distributions
+# This script is intended to be used as a root user
+# This script should be ideally invoqued by a Cloud-init script 
+# 	Read more at: https://www.digitalocean.com/community/tutorials/an-introduction-to-cloud-config-scripting#run-arbitrary-commands-for-more-control
+# 	
+# Original Gist at: https://gist.github.com/baamenabar/2a825178318d27fc20abfe5a413b45eb
+# Author B. Agustin Amenabar L. @iminabar
+# 
+
+# The user must be the same set by the Cloud-init script
+nixusr="leopold"
+
+# We go in to the user's home folder and copy the authorized keys from root
+cd /home/$nixusr/
+if [ ! -d ".ssh" ]; then
+  mkdir .ssh
+fi
+echo $(pwd)
+chmod 777 .ssh
+cat /root/.ssh/authorized_keys > .ssh/authorized_keys
+chown -R $nixusr:$nixusr .ssh
+chmod 600 .ssh/authorized_keys
+chmod 700 .ssh
+
+# We use sed to remove the possibility to log in as root user and to log in withour password.
+sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+echo 'reconfigured sshd_config'
+
+# we restart the ssh service to make the changes effective
+service ssh restart
+echo 'restarted ssh'
+echo "You can now login with the user: $nixusr"
+
+# We add the user to the docker group (the docker installation creates this group)
+sudo usermod -aG docker $nixusr
+
+# We clone the flyimg repo into the user's folder.
+echo "cloning flyimg into"
+pwd
+sudo -HEu $nixusr git clone https://github.com/sadok-f/flyimg.git /home/$nixusr/flyimg
+
+# List the repo's content to comfirm it's there.
+cd flyimg
+echo "...cloned! content is:"
+ls -la
+
+# Build the docker container
+echo "sudo -u $nixusr docker build -t flyimg ."
+sudo -u $nixusr docker build -t flyimg .
+sleep 5
+
+# Run the container, naming it "flyimg" and exposing it through port 80
+echo "sudo -u $nixusr docker run -t -d -i -p 80:80 -v /home/$nixusr/flyimg:/var/www/html --name flyimg flyimg"
+sudo -u $nixusr docker run -t -d -i -p 80:80 -v /home/$nixusr/flyimg:/var/www/html --name flyimg flyimg
+sleep 5
+
+# Update the container to restart always in case of stopping for any reason.
+# Even after the server has restarted
+echo "sudo -u $nixusr docker update --restart=always flyimg"
+sudo -u $nixusr docker update --restart=always flyimg
+
+# list the container(s)
+echo "sudo -u $nixusr docker ps"
+sudo -u $nixusr docker ps
+sleep 10
+
+# Run composer inside the container image to download all the dependencies the application needs.
+echo "sudo -HEu $nixusr docker exec -i flyimg composer install"
+sudo -HEu $nixusr docker exec -i flyimg composer install
+
+echo $'\n Horray! Provisioning finished \n Happy Imaging.'
